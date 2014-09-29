c1ff8f4a4c32f8aa10c11c456ad116391bce0b27
josacord92 <josacord92@gmail.com>
2014-09-28 20:42:12 -0600

    Se agregan heroes
    heroes con los mismos nombres de los heroes de age of mythology es lo
    que se me ocurrio. Asi ya se completan las clases necesarias para los
    personajes jugables, practicamente es el cascaron pero ya asi tenemos
    idea de como ir trabajando y dandole forma

---
+++ src/Capa_Logica/Hercules.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Capa_Logica;
+
+/**
+ *
+ * @author Jose
+ */
+public class Hercules extends Heroe {
+    
+    public static double costo = 1000;
+    public static int nivelAparicion = 2;
+    public static int campoEnElEjercito = 1;
+    
+    Hercules(){
+    setID(this.getIDCounter());        // Se le asigna un identificador primary KEY al espadachin como identificador unico
+    this.incrementarIDCounter();       // Incremento del primary key para la creacion de otros elementos en el juego    
+    setNombre("Hercules -ID: "+getID());   //Nombre del Personaje
+    setNivel(1);                       //Nivel de fuerza Con El Que Se Crea El Guerrero - Inicia en nivel 1
+    setCantidadGolpesPorUnidadTiempo(800);
+    setVida(1000);
+    }
+    
+    public void run()
+    {
+        /*
+        // es lo que hilo va a hacer
+        while (!detener)
+        {
+            
+            Point punto = refPantalla.lblMovimiento.getLocation();
+            
+            refPantalla.lblMovimiento.setLocation(punto.x + 10, punto.y);
+            
+            int velocidad = Integer.parseInt(refPantalla.txfVelocidad.getText())*1000;
+            
+            
+            
+            try {
+                sleep(velocidad);
+            } catch (InterruptedException ex) {
+            
+            }
+            
+            while (pausa)
+            {
+                try {
+                    sleep(100);
+                } catch (InterruptedException ex) {
+                    //Logger.getLogger(HiloMover.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        */
+        
+    
+    }
+    public void atacar(){
+    
+    }
+    
+    public void mover(){
+    
+    }
+}
